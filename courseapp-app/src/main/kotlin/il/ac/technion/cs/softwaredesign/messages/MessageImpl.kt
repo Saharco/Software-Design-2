fa9b69b00303a59409ea7f65bfd4ca1f3a51dd5e
Sahar <saharco@campus.technion.ac.il>
2019-06-11 18:53:07 +0300

    nearly done implementing messages: passing all staff tests

--- courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/messages/MessageImpl.kt
+++ courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/messages/MessageImpl.kt
@@ -7,10 +7,9 @@ import java.io.ObjectOutputStream
 import java.io.Serializable
 import java.time.LocalDateTime
 
-
 class MessageImpl(override val id: Long, override val media: MediaType, override val contents: ByteArray,
-                  override val created: LocalDateTime, override var received: LocalDateTime? = null,
-                  var sender: String? = null, var usersCount: Long = 0) : Message, Serializable {
+                  override val created: LocalDateTime, var usersCount: Long = 0,
+                  override var received: LocalDateTime? = null, var sender: String? = null) : Message, Serializable {
 
     companion object {
 
@@ -18,17 +17,21 @@ class MessageImpl(override val id: Long, override val media: MediaType, override
         private val charset = Charsets.UTF_8
 
         fun deserialize(string: String): MessageImpl {
-            return ObjectInputStream(ByteArrayInputStream(string.toByteArray())).readObject() as MessageImpl
+            val bais = ByteArrayInputStream(string.toByteArray(charset("ISO-8859-1")))
+            val ois = ObjectInputStream(bais)
+            return ois.readObject() as MessageImpl
         }
     }
 
     constructor(msg: MessageImpl)
-            : this(msg.id, msg.media, msg.contents, msg.created, msg.received, msg.sender, msg.usersCount)
+            : this(msg.id, msg.media, msg.contents, msg.created, msg.usersCount, msg.received, msg.sender)
 
     fun serialize(): String {
-        val outputStream = ByteArrayOutputStream()
-        ObjectOutputStream(outputStream).writeObject(this)
-        return outputStream.toString(charset)
+        val baos = ByteArrayOutputStream()
+        val oos = ObjectOutputStream(baos)
+        oos.writeObject(this)
+        oos.close()
+        return baos.toString("ISO-8859-1")
     }
 
     fun isDonePending(): Boolean {

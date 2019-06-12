172f7f67c957e2a5f6c14ebdf63b65f7ea503e29
Sahar <saharco@campus.technion.ac.il>
2019-06-12 22:25:55 +0300

    about to change guice

--- courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseAppModule.kt
+++ courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseAppModule.kt
@@ -17,20 +17,8 @@ import java.util.concurrent.CompletableFuture
 
 class CourseAppModule : KotlinModule() {
 
-    /*
-     * Replace with these two when running the tests with mocks:
-     *
-     * --- TO THE STAFF: ---
-     *  we are pretty sure this is what you meant, but this means that at the time of submission:
-     *  our tests do not run anymore because we don't have the real SecureStorage implementation
-     * ---------------------
-     *
-     * private val factory = SecureStorageFactoryMock()
-     * private val dbFactory = CourseAppDatabaseFactory(factory)
-     *
-     */
-
-    private val factory = SecureStorageFactoryMock() //TODO: change this when submitting
+    //TODO: check if this needs to change when submitting
+    private val factory = SecureStorageFactoryMock()
     private val dbFactory = CourseAppDatabaseFactory(factory)
 
     override fun configure() {
